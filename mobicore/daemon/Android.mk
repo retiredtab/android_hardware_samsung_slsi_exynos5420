57f566dc36753f1a64bdc04f5c67688ec2690c7f
Erfan Abdi <erfangplus@gmail.com>
2019-09-04 17:20:32 +0430

    exynos5420: Drop module eng tags
    Change-Id: I2e84d598b2412e617e5575bfdefd804612f76889

--- mobicore/daemon/Android.mk
+++ mobicore/daemon/Android.mk
@@ -10,7 +10,7 @@ LOCAL_PATH := $(call my-dir)
 # =============================================================================
 include $(CLEAR_VARS)
 LOCAL_MODULE := libMcClient
-LOCAL_MODULE_TAGS := debug eng optional
+LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES += $(GLOBAL_INCLUDES)
 LOCAL_SHARED_LIBRARIES += $(GLOBAL_LIBRARIES) liblog
 
@@ -48,7 +48,7 @@ include $(BUILD_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := mcDriverDaemon
-LOCAL_MODULE_TAGS := debug eng optional
+LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS += -include buildTag.h
 LOCAL_CFLAGS += -DLOG_TAG=\"McDaemon\"
 LOCAL_C_INCLUDES += $(GLOBAL_INCLUDES)
@@ -87,7 +87,7 @@ include $(BUILD_EXECUTABLE)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := libMcRegistry
-LOCAL_MODULE_TAGS := debug eng optional
+LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS += -DLOG_TAG=\"McRegistry\"
 LOCAL_C_INCLUDES += $(GLOBAL_INCLUDES)
 LOCAL_SHARED_LIBRARIES += $(GLOBAL_LIBRARIES) liblog
@@ -116,7 +116,7 @@ include $(BUILD_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := libPaApi
-LOCAL_MODULE_TAGS := debug eng optional
+LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS += -DLOG_TAG=\"PaApi\"
 LOCAL_C_INCLUDES += $(GLOBAL_INCLUDES)
 LOCAL_SHARED_LIBRARIES += $(GLOBAL_LIBRARIES) liblog
